b2ae2914876345fb8294cb7efe2a9fe70743fafa
id4m0 <voxel248@gmail.com>
2020-02-12 21:34:49 +1100

    Refactor CMake Code Generator
    Pulled adding new path to list into it's own function so it can be used for the other Cmake files.
    Added tests for this new function.
    Formatting
    Added cmake file name constant, as cmake files are all names the same

--- code_generation/cmake_code_generator.py
+++ code_generation/cmake_code_generator.py
@@ -3,6 +3,8 @@ import re
 
 from . import code_generator_util
 
+CMAKE_FILE_NAME = 'CMakeLists.txt'
+
 
 class CMakeCodeManager:
     """Adds new files to Cmake build list"""
@@ -11,39 +13,48 @@ class CMakeCodeManager:
         self._source_path = gui.get_source_path()
         self._node_name = gui.get_node_name()
 
+    def _insert_cmake_file_path(self, names_start_i, file_text, new_file_path):
+        """
+        Inserts the new file path into the file text and returns the modified text
+        :param names_start_i: start index of the list of filenames in text
+        :param file_text: file text
+        :param new_file_name: file path to be inserter
+        :return: file text with new path inserted
+        """
+        for i in range(names_start_i, len(file_text)):
+            if file_text[i] == ')':
+                break
+        else:
+            raise Exception("End of svm not found")
+        names_end_i = i - 1
+        file_paths = file_text[names_start_i:names_end_i].split('\n')
+
+        # Try to place new file for sorted order, however not all file names are sorted alphabetically,
+        # Best that can be done is to place before the first name greater than new file name
+        for i, file_name in enumerate(file_paths):
+            if file_name > new_file_path:
+                break
+        else:
+            # If element should go last
+            i = len(file_paths)
+        file_paths.insert(i, new_file_path)
+
+        return file_text[:names_start_i] + '\n'.join(file_paths) + file_text[names_end_i:]
+
     def _add_svm(self):
         """Adds created svm file to cmake list"""
-        with open(path.join(self._source_path, "intern", "cycles", "kernel", "CMakeLists.txt"), 'r+') as f:
+        with open(path.join(self._source_path, "intern", "cycles", "kernel", CMAKE_FILE_NAME), 'r+') as f:
             text = f.read()
-            match = re.search('set\(SRC_SVM_HEADERS', text)
+            match = re.search(r'set\(SRC_SVM_HEADERS', text)
             if not match:
                 raise Exception("Match not found")
 
             svm_start = match.end() + 1
 
-            for i in range(svm_start, len(text)):
-                if text[i] == ')':
-                    break
-            else:
-                raise Exception("End of svm not found")
-            svm_end = i - 1
-            svm_files = text[svm_start:svm_end]
-
-            svm_file_name = '  svm/svm_{name}.h'.format(
+            svm_file_path = '  svm/svm_{name}.h'.format(
                 name=code_generator_util.string_lower_underscored(self._node_name))
-            svm_files = svm_files.split('\n')
 
-            # Try to place new file for sorted order, however not all file names are sorted alphabetically,
-            # Best that can be done is to place before the first name greater than new file name
-            for i, file_name in enumerate(svm_files):
-                if file_name > svm_file_name:
-                    break
-            else:
-                # If element should go last
-                i = len(svm_files)
-            svm_files.insert(i, svm_file_name)
-
-            text = text[:svm_start] + '\n'.join(svm_files) + text[svm_end:]
+            text = self._insert_cmake_file_path(svm_start, text, svm_file_path)
 
             f.seek(0)
             f.write(text)
--- tests/test_cmake_code_generator.py
+++ tests/test_cmake_code_generator.py
@@ -18,6 +18,223 @@ class TestCMake(unittest.TestCase):
     def _create_default_cmake_manager(self):
         return CMakeCodeManager(self.mock_gui)
 
+    def test_insert_cmake_middle_sorted_path_correct_formatting(self):
+        """Insert a path which alphabetically will fall in the middle of the file list"""
+        cmake = self._create_default_cmake_manager()
+        file_text = 'closure/bsdf_principled_sheen.h\n' \
+                    '  closure/bsdf_hair_principled.h\n' \
+                    ')\n' \
+                    '\n' \
+                    'set(SRC_SVM_HEADERS\n' \
+                    '  svm/svm.h\n' \
+                    '  svm/svm_ao.h\n' \
+                    '  svm/svm_aov.h\n' \
+                    '  svm/svm_attribute.h\n' \
+                    '  svm/svm_bevel.h\n' \
+                    '  svm/svm_blackbody.h\n' \
+                    '  svm/svm_bump.h\n' \
+                    '  svm/svm_camera.h\n' \
+                    '  svm/svm_clamp.h\n' \
+                    '  svm/svm_closure.h\n' \
+                    '  svm/svm_convert.h\n' \
+                    '  svm/svm_checker.h\n' \
+                    '  svm/svm_color_util.h\n' \
+                    '  svm/svm_brick.h\n' \
+                    '  svm/svm_displace.h\n' \
+                    '  svm/svm_fresnel.h\n' \
+                    '  svm/svm_wireframe.h\n' \
+                    '  svm/svm_wavelength.h\n' \
+                    '  svm/svm_gamma.h\n' \
+                    '  svm/svm_brightness.h\n' \
+                    '  svm/svm_geometry.h\n' \
+                    '  svm/svm_gradient.h\n' \
+                    '  svm/svm_hsv.h\n' \
+                    '  svm/svm_ies.h\n' \
+                    '  svm/svm_image.h\n' \
+                    '  svm/svm_invert.h\n' \
+                    '  svm/svm_light_path.h\n' \
+                    '  svm/svm_magic.h\n' \
+                    '  svm/svm_map_range.h\n' \
+                    '  svm/svm_mapping.h\n' \
+                    '  svm/svm_mapping_util.h\n' \
+                    '  svm/svm_math.h\n' \
+                    '  svm/svm_math_util.h\n' \
+                    '  svm/svm_mix.h\n' \
+                    '  svm/svm_musgrave.h\n' \
+                    '  svm/svm_noise.h\n' \
+                    '  svm/svm_noisetex.h\n' \
+                    '  svm/svm_normal.h\n' \
+                    '  svm/svm_ramp.h\n' \
+                    '  svm/svm_ramp_util.h\n' \
+                    '  svm/svm_sepcomb_hsv.h\n' \
+                    '  svm/svm_sepcomb_vector.h\n' \
+                    '  svm/svm_sky.h\n' \
+                    '  svm/svm_tex_coord.h\n' \
+                    '  svm/svm_truchet.h\n' \
+                    '  svm/svm_fractal_noise.h\n' \
+                    '  svm/svm_types.h\n' \
+                    '  svm/svm_value.h\n' \
+                    '  svm/svm_vector_transform.h\n' \
+                    '  svm/svm_voronoi.h\n' \
+                    '  svm/svm_voxel.h\n' \
+                    '  svm/svm_wave.h\n' \
+                    '  svm/svm_white_noise.h\n' \
+                    '  svm/svm_vertex_color.h\n' \
+                    ')\n' \
+                    '\n' \
+                    'set(SRC_GEOM_HEADERS\n'
+        text = cmake._insert_cmake_file_path(len('closure/bsdf_principled_sheen.h\n  closure/bsdf_hair_principled.h\n'
+                                                 ')\n\n'
+                                                 'set(SRC_SVM_HEADERS)'), file_text, '  svm/svm_node_name.h')
+
+        self.assertTrue('  svm/svm_fresnel.h\n'
+                        '  svm/svm_node_name.h\n'
+                        '  svm/svm_wireframe.h\n' in text)
+
+    def test_insert_cmake_first_sorted_path_correct_formatting(self):
+        """Insert a path which alphabetically will fall at the start of the file list"""
+        cmake = self._create_default_cmake_manager()
+        file_text = 'closure/bsdf_principled_sheen.h\n' \
+                    '  closure/bsdf_hair_principled.h\n' \
+                    ')\n' \
+                    '\n' \
+                    'set(SRC_SVM_HEADERS\n' \
+                    '  svm/svm.h\n' \
+                    '  svm/svm_ao.h\n' \
+                    '  svm/svm_aov.h\n' \
+                    '  svm/svm_attribute.h\n' \
+                    '  svm/svm_bevel.h\n' \
+                    '  svm/svm_blackbody.h\n' \
+                    '  svm/svm_bump.h\n' \
+                    '  svm/svm_camera.h\n' \
+                    '  svm/svm_clamp.h\n' \
+                    '  svm/svm_closure.h\n' \
+                    '  svm/svm_convert.h\n' \
+                    '  svm/svm_checker.h\n' \
+                    '  svm/svm_color_util.h\n' \
+                    '  svm/svm_brick.h\n' \
+                    '  svm/svm_displace.h\n' \
+                    '  svm/svm_fresnel.h\n' \
+                    '  svm/svm_wireframe.h\n' \
+                    '  svm/svm_wavelength.h\n' \
+                    '  svm/svm_gamma.h\n' \
+                    '  svm/svm_brightness.h\n' \
+                    '  svm/svm_geometry.h\n' \
+                    '  svm/svm_gradient.h\n' \
+                    '  svm/svm_hsv.h\n' \
+                    '  svm/svm_ies.h\n' \
+                    '  svm/svm_image.h\n' \
+                    '  svm/svm_invert.h\n' \
+                    '  svm/svm_light_path.h\n' \
+                    '  svm/svm_magic.h\n' \
+                    '  svm/svm_map_range.h\n' \
+                    '  svm/svm_mapping.h\n' \
+                    '  svm/svm_mapping_util.h\n' \
+                    '  svm/svm_math.h\n' \
+                    '  svm/svm_math_util.h\n' \
+                    '  svm/svm_mix.h\n' \
+                    '  svm/svm_musgrave.h\n' \
+                    '  svm/svm_noise.h\n' \
+                    '  svm/svm_noisetex.h\n' \
+                    '  svm/svm_normal.h\n' \
+                    '  svm/svm_ramp.h\n' \
+                    '  svm/svm_ramp_util.h\n' \
+                    '  svm/svm_sepcomb_hsv.h\n' \
+                    '  svm/svm_sepcomb_vector.h\n' \
+                    '  svm/svm_sky.h\n' \
+                    '  svm/svm_tex_coord.h\n' \
+                    '  svm/svm_truchet.h\n' \
+                    '  svm/svm_fractal_noise.h\n' \
+                    '  svm/svm_types.h\n' \
+                    '  svm/svm_value.h\n' \
+                    '  svm/svm_vector_transform.h\n' \
+                    '  svm/svm_voronoi.h\n' \
+                    '  svm/svm_voxel.h\n' \
+                    '  svm/svm_wave.h\n' \
+                    '  svm/svm_white_noise.h\n' \
+                    '  svm/svm_vertex_color.h\n' \
+                    ')\n' \
+                    '\n' \
+                    'set(SRC_GEOM_HEADERS\n'
+        text = cmake._insert_cmake_file_path(len('closure/bsdf_principled_sheen.h\n  closure/bsdf_hair_principled.h\n'
+                                                 ')\n\n'
+                                                 'set(SRC_SVM_HEADERS)'), file_text, '  a')
+
+        self.assertTrue('  a\n'
+                        '  svm/svm.h\n' in text)
+
+    def test_insert_cmake_last_sorted_path_correct_formatting(self):
+        """Insert a path which alphabetically will fall at the end of the file list"""
+        cmake = self._create_default_cmake_manager()
+        file_text = 'closure/bsdf_principled_sheen.h\n' \
+                    '  closure/bsdf_hair_principled.h\n' \
+                    ')\n' \
+                    '\n' \
+                    'set(SRC_SVM_HEADERS\n' \
+                    '  svm/svm.h\n' \
+                    '  svm/svm_ao.h\n' \
+                    '  svm/svm_aov.h\n' \
+                    '  svm/svm_attribute.h\n' \
+                    '  svm/svm_bevel.h\n' \
+                    '  svm/svm_blackbody.h\n' \
+                    '  svm/svm_bump.h\n' \
+                    '  svm/svm_camera.h\n' \
+                    '  svm/svm_clamp.h\n' \
+                    '  svm/svm_closure.h\n' \
+                    '  svm/svm_convert.h\n' \
+                    '  svm/svm_checker.h\n' \
+                    '  svm/svm_color_util.h\n' \
+                    '  svm/svm_brick.h\n' \
+                    '  svm/svm_displace.h\n' \
+                    '  svm/svm_fresnel.h\n' \
+                    '  svm/svm_wireframe.h\n' \
+                    '  svm/svm_wavelength.h\n' \
+                    '  svm/svm_gamma.h\n' \
+                    '  svm/svm_brightness.h\n' \
+                    '  svm/svm_geometry.h\n' \
+                    '  svm/svm_gradient.h\n' \
+                    '  svm/svm_hsv.h\n' \
+                    '  svm/svm_ies.h\n' \
+                    '  svm/svm_image.h\n' \
+                    '  svm/svm_invert.h\n' \
+                    '  svm/svm_light_path.h\n' \
+                    '  svm/svm_magic.h\n' \
+                    '  svm/svm_map_range.h\n' \
+                    '  svm/svm_mapping.h\n' \
+                    '  svm/svm_mapping_util.h\n' \
+                    '  svm/svm_math.h\n' \
+                    '  svm/svm_math_util.h\n' \
+                    '  svm/svm_mix.h\n' \
+                    '  svm/svm_musgrave.h\n' \
+                    '  svm/svm_noise.h\n' \
+                    '  svm/svm_noisetex.h\n' \
+                    '  svm/svm_normal.h\n' \
+                    '  svm/svm_ramp.h\n' \
+                    '  svm/svm_ramp_util.h\n' \
+                    '  svm/svm_sepcomb_hsv.h\n' \
+                    '  svm/svm_sepcomb_vector.h\n' \
+                    '  svm/svm_sky.h\n' \
+                    '  svm/svm_tex_coord.h\n' \
+                    '  svm/svm_truchet.h\n' \
+                    '  svm/svm_fractal_noise.h\n' \
+                    '  svm/svm_types.h\n' \
+                    '  svm/svm_value.h\n' \
+                    '  svm/svm_vector_transform.h\n' \
+                    '  svm/svm_voronoi.h\n' \
+                    '  svm/svm_voxel.h\n' \
+                    '  svm/svm_wave.h\n' \
+                    '  svm/svm_white_noise.h\n' \
+                    '  svm/svm_vertex_color.h\n' \
+                    ')\n' \
+                    '\n' \
+                    'set(SRC_GEOM_HEADERS\n'
+        text = cmake._insert_cmake_file_path(len('closure/bsdf_principled_sheen.h\n  closure/bsdf_hair_principled.h\n'
+                                                 ')\n\n'
+                                                 'set(SRC_SVM_HEADERS)'), file_text, '  z')
+
+        self.assertTrue('  svm/svm_vertex_color.h\n'
+                        '  z\n' in text)
+
     def test_write_svm_cmake_middle_sorted_name_correct_formatting(self):
         """Insert a name which alphabetically will fall in the middle of the file list"""
         with patch('builtins.open', mock.mock_open(read_data=
